1fb1e004d1ac9460a12403ece4816e9b8c5aebbf
sarita <sdonairec@gmail.com>
2023-01-31 15:40:47 +0100

    adding model w/schema and make endpoint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+
 db.once('open', function() {
     console.log("Connected to MongoDB");
 })
@@ -15,25 +15,36 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/monsters');
   
 }
-const monsters=[
-    { id: 1, name:"Albert", level: 5, description: "La vipere"},
-    { id: 2, name:"Pilou", level: 3, description: "Le Loup garou"}, 
-    { id: 3, name:"Mounir", level:2, description: "La vampire"},
-];
+app.use(express.urlencoded({ extended: true }));
+
+const Monster = require('./models/monster');
+
+// const monsters=[
+//     { id: 1, name:"Albert", level: 5, description: "La vipere"},
+//     { id: 2, name:"Pilou", level: 3, description: "Le Loup garou"}, 
+//     { id: 3, name:"Mounir", level:2, description: "La vampire"},
+// ];
 
 app.get('/', (req,res) => {
-    res.send(monsters)
+    Monster.find({}, (err, monsters) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(monsters)
+    }
+}) 
 })
 
-app.post('/add', (req,res) => {
-monsters.push({
-    id:monsters[monster.length-1].id+1,
-    name: req.query.name,
-    level: parseInt(req.query.level),
-    description: req.query.description,
-});
-console.log(req.body);
-res.send("OK");
+app.post("/add", (req, res) => {
+  let newMonster = new Monster(req.body);
+  console.log(req.body);
+  newMonster.save((error, monster) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(monster);
+    }
+  });
 });
 
 app.get("/:id", (req,res) => {
